0065b5eb157a908d1cc7a0cb154a0a8c887bf11d
FirLanPro@yandex <FirLanPro@yandex.ru>
2023-05-20 20:42:15 +0300

    4 h_01

---
+++ 4_four/h_01.py
@@ -0,0 +1,15 @@
+# Найдите сумму цифр трехзначного числа.
+# 123 -> 6 (1 + 2 + 3)
+# 100 -> 1 (1 + 0 + 0) 
+
+number = input('введите число: ')
+summa = 0 
+length= len(number)
+k = range(length)
+if length == 3:
+    for i in k:
+        s = int(number[i])
+        summa += s
+    print(summa) 
+else: print('не трёхзначное число')
+
