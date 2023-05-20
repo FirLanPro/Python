58bacd9871b0ee845a1311c4095ed40b981142f6
FirLanPro@yandex <FirLanPro@yandex.ru>
2023-05-20 23:47:36 +0300

    4 h_04

--- 4_four/h_04.py
+++ 4_four/h_04.py
@@ -4,6 +4,25 @@
 # если разрешается сделать один разлом по прямой между дольками 
 # (то есть разломить шоколадку на два прямоугольника).
 
-# 3 2 4 -> yes
+# 3 2 4 -> yes 
 # 3 2 1 -> no
+
+# общее количество долек: p
+# отломить K долек
+
+
+n = int(input('n:'))
+m = int(input('m:'))
+k = int(input('k:'))
+if n != 0 and m != 0:
+    if k % 2 == 0:
+        if n == k or m == k:
+            print("yes")
+        elif n*m-k>0:
+            print("yes")
+        k = k + 1
+    elif n == k or m == k:
+        print("yes")
+    else: print("no")
+else: print("шоколада нет")
 
