863ffcaeb9bee280f19c4c18e4efcaf31ee75b57
FirLanPro@yandex <FirLanPro@yandex.ru>
2023-05-26 21:14:47 +0300

    5 k1-k7

---
+++ 5_five/k_02.py
@@ -0,0 +1,13 @@
+# добавление элемента в конец списка
+list_1 = [1, 5]
+print(list_1)
+list_1.append(8) # добавление элемента в конец списка
+print(list_1)
+list_1.append(343) 
+print(list_1)
+
+# добавление элементов в список через цикл
+list_1 = []
+for i in range(5):
+    list_1.append(i)
+    print(list_1)
---
+++ 5_five/k_05.py
@@ -0,0 +1,18 @@
+# кортежи. неизменяемые элементы.
+
+t = () # класс кортеж
+print(type(t))
+
+t = (1) # класс int
+print(type(t))
+
+t = (1,3,4,) # класс кортеж
+print(type(t))
+
+# список поменять на кортеж
+v = [1,2,3,4,4]
+print(v)
+print(type(v))
+v = tuple(v)
+print(v)
+print(type(v))
