3af0f0ba804e7c8d2abf9ddf7d06f0d98bfee585
FirLanPro@yandex <FirLanPro@yandex.ru>
2023-05-28 01:34:01 +0300

    8 t2 редактиров

--- 8_eight/t_02.py
+++ 8_eight/t_02.py
@@ -10,8 +10,30 @@
 #     6
 #     -> 5
 
-n = int(input('количество элементов в массиве: '))
-x = int(input('некоторое число X: '))
+import random
 
-for i in range(1, n+1):
-    print(i)
+n = int(input('количество элементов в массиве: '))
+list_1 = [random.randint(1,10) for i in range(1, n+1) if n > 0]
+print(*list_1)
+x = int(input('некоторое число X: '))
+count = 0
+count_1 = 0
+for i in range(n):
+   if  count < list_1[i] < x:
+        count = list_1[i]
+   if count < list_1[i] > x:
+        count_1 = list_1[i]
+
+m = (count_1-count) // 2
+for i in range(1,m+1):
+    if x-i != count:
+        print('4444',count)
+    if x+i != count_1:
+        print('555',count_1)
+    if  x+1 == x-1:
+        print('666',count_1,count)
+
+print('111',m)
+print('2222',count)
+print('3333',count_1)
+
