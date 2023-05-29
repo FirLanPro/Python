a891d2c4978ba3f8ce78479cf83bf1350d362214
FirLanPro@yandex <FirLanPro@yandex.ru>
2023-05-29 21:33:36 +0300

    8 t2 редакт

--- 8_eight/t_02.py
+++ 8_eight/t_02.py
@@ -1,8 +1,8 @@
-#  Требуется найти в массиве A[1..N] 
-#  самый близкий по величине элемент к заданному числу X. 
+#  Требуется найти в массиве A[1..N]
+#  самый близкий по величине элемент к заданному числу X.
 #  Пользователь в первой строке
-#  вводит натуральное число N – количество элементов в массиве. 
-#  В последующих  строках записаны N целых чисел Ai. 
+#  вводит натуральное число N – количество элементов в массиве.
+#  В последующих  строках записаны N целых чисел Ai.
 #  Последняя строка содержит число X
 
 # 5
@@ -10,34 +10,46 @@
 #     6
 #     -> 5
 
-import random
+# если ближайших элемента 2, выводи оба(+-1, +-2, и т.д.)
+# 1 способ:
+# import random
+# n = int(input('количество элементов в массиве: '))
+# list_1 = [random.randint(1,10) for i in range(1, n+1) if n > 0]
+# print(*list_1)
+# x = int(input('некоторое число X: '))
+# count = 1
 
-number = 10
+# list_2 = []
+# for j in range(count+1):
+#     for i in range(n):
+#         if  x - count == list_1[i] >0 :
+#             list_2.append(list_1[i])
+#         if x + count == list_1[i]:
+#             list_2.append(list_1[i])
+#     if len(list_2) == 0 or : #####
+#         count += 1
+        
+# print(set(list_2))
+
+
+# 2 способ:
+
+import random
 n = int(input('количество элементов в массиве: '))
-list_1 = [random.randint(1,number) for i in range(1, n+1) if n > 0]
+list_1 = [random.randint(1,10) for i in range(1, n+1) if n > 0]
 print(*list_1)
 x = int(input('некоторое число X: '))
-
 count = 1
 count_1 = 0
-
-
 for j in range(count+1):
-    for i in range(n-1):
-        if x - count == list_1[i]:
+    for i in range(n):
+        if x - count == list_1[i] > 0 :
             count_1 = list_1[i]
         if x + count == list_1[i]:
             count_1 = list_1[i]
     if count_1 == 0:
-        count += 1      
+        count += 1
 print(count_1)
-      
-
-# if count_1 != 0 and count_2 != 0:
-#     print("2 ближайших по величине элемента")       
-
-
-
 
 
 
